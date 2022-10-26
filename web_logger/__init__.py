2ea831dad8070ab504db4323d7e1c581b62b3b5f
NicoEvZ <35323277+NicoEvZ@users.noreply.github.com>
2022-10-26 21:45:01 +1300

    Change to using FastAPI

--- web_logger/__init__.py
+++ web_logger/__init__.py
@@ -1,44 +1,37 @@
-from flask import Flask, jsonify, Response, url_for, send_from_directory, render_template, Markup, make_response, request, Blueprint
-from flask_restful import Api, Resource, reqparse
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from .Devices.Arduino import Arduino
 import logging
-import asyncio
-app = Flask(__name__)
-api = Api(app)
+app = FastAPI
 
 devices = [Arduino('/dev/ttyUSB0')]
 logging.info("Added arduino to device list")
 
-@app.route("/")
+@app.get("/")
 def home():
-    return render_template("home.html")
+    return FileResponse("home.html")
 
-class DeviceList(Resource):    
-    def get(self):
-        """Gives a list of all devices and their dimensions"""
-        device_dim = {}
-        for dev in devices:
-            device_dim[dev.name] = dev.get_dimensions()
-        return {'Devices': device_dim}
+@app.get("/Devices")
+def ListDevices():
+    """Gives a list of all devices and their dimensions"""
+    device_dim = {}
+    for dev in devices:
+        device_dim[dev.name] = dev.get_dimensions()
+    return {'Devices': device_dim}
 
-class DeviceDims(Resource):
-    def get(self, id):
-        """Gives dimensions and current values for device by id (index of devices list)"""
-        return devices[id].get_current_value()
+@app.get("/Devices/{device_id}")
+def ListDeviceIDs(device_id: int):
+    """Gives dimensions and current values for device by id (index of devices list)"""
+    return devices[device_id].get_current_value()
 
-class DeviceUnits(Resource):
-    def get(self, id):
-        return devices[id].get_units()
+@app.get("/Devices/{device_id}/units")
+def ListDeviceUnits(device_id: int):
+    """Gives units for device by id (index of devices list)"""
+    return devices[device_id].get_units()
 
-class TimeSeries(Resource):
-    def get(self, id, n):
-        return devices[id].get_time_series(n)
-
-
-api.add_resource(DeviceList, '/Devices')
-api.add_resource(DeviceDims, '/Devices/<int:id>')
-api.add_resource(DeviceUnits, '/Devices/<int:id>/units')
-api.add_resource(TimeSeries, '/Devices/<int:id>/n=<int:n>')
+@app.get("/Devices/{device_id}/{n}")
+def TimeSeries(device_id: int, n : int):
+    return devices[device_id].get_time_series(n)
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(message)s', filename='example.log', level=logging.DEBUG)
